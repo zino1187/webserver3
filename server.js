cdff7727cd3e409638550ebdf868b6165c34f164
z <zino1187@gmail.com>
2018-08-04 13:41:13 +0900

    ver 1.4

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var http=require("http");
 var express=require("express"); 
 var ejs=require("ejs");// 동적 html 생성 모듈...
 var fs=require("fs");//파일 읽어들이는 모듈!!
+var oracledb=require("oracledb");
 
 var app=express();//모듈생성
 
@@ -16,40 +17,60 @@ var app=express();//모듈생성
 // 어떤효과? - 정적파일들을 일일이 라우팅 시킬 필요없다...
 app.use(express.static(__dirname)); 
 
+var conn;
+
+//오라클 접속 
+oracledb.getConnection({
+	user:"node",
+	password:"node",
+	connectString:"localhost/XE"
+}, function(error, con){
+	if(error){
+		console.log("접속에 실패!!", error);
+	}
+	conn=con;
+});
+
+
 //리스트 요청받기
 app.get("/list", function(request, response){
 	//클라이언트가 전송한 get방식의 데이터를 request 객체에서 끄집어 내자!!
 	console.log(request.query.currentPage);
 
-	//페이징 처리 기법
-	var currentPage=1;		//현재 보고 있는 페이지
 	//사용자가 링크를 누른 경우엔...넘어온 currentPage 값으로 대체!!
 	if(request.query.currentPage != undefined){
 		currentPage = parseInt(request.query.currentPage);
 	}
-	var totalRecord=26;	//총 레코드 수	
-	var pageSize=10;		//페이지당 보여질 레코드 수
-	var totalPage=Math.ceil(totalRecord/pageSize);
-	var blockSize=10;		//블럭당 보여질 페이지 수
-	var firstPage=currentPage-(currentPage-1)%blockSize;//블럭당 시작페이지
-	var lastPage=firstPage + (blockSize-1);//블럭당 마지막 페이지
-	var num=totalRecord - ((currentPage-1)*pageSize);//페이지당 시작 게시물 갯수 번호
 
-	fs.readFile("list.ejs","utf-8", function(error, data){
-		if(error){
-			console.log("읽기실패", error);
-		}
-		response.writeHead(200, {"Content-Type":"text/html"});
-		response.end(ejs.render(data, {
-			currentPage:currentPage,
-			totalRecord:totalRecord,
-			pageSize:pageSize,
-			totalPage:totalPage,
-			blockSize:blockSize,
-			firstPage:firstPage,
-			lastPage:lastPage,
-			num:num
-		}));
+	conn.execute("select * from notice order by notice_id desc", function(err, result, fields){
+		if(err){
+			console.log("조회실패!!");
+		}	
+		var currentPage=1;		//현재 보고 있는 페이지
+		var totalRecord=result.rows.length;
+		var pageSize=10;		//페이지당 보여질 레코드 수
+		var totalPage=Math.ceil(totalRecord/pageSize);
+		var blockSize=10;		//블럭당 보여질 페이지 수
+		var firstPage=currentPage-(currentPage-1)%blockSize;//블럭당 시작페이지
+		var lastPage=firstPage + (blockSize-1);//블럭당 마지막 페이지
+		var num=totalRecord - ((currentPage-1)*pageSize);//페이지당 시작 게시물 갯수 번호
+
+		fs.readFile("list.ejs","utf-8", function(error, data){
+			if(error){
+				console.log("읽기실패", error);
+			}
+			response.writeHead(200, {"Content-Type":"text/html"});
+			response.end(ejs.render(data, {
+				currentPage:currentPage,
+				totalRecord:totalRecord,
+				pageSize:pageSize,
+				totalPage:totalPage,
+				blockSize:blockSize,
+				firstPage:firstPage,
+				lastPage:lastPage,
+				num:num
+			}));
+		});
 	});
 });
 
